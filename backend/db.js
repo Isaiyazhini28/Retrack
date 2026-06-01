439a9d99a4ef8f58ef9da8d06f59b5a8edfba0f9
Isaiyazhini28 <isai12d25@gmail.com>
2026-06-01 10:47:30 +0530

    fix db connection to use aiven

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,18 @@
 import mysql from "mysql2/promise";
+import fs from "fs";
+import dotenv from "dotenv";
+dotenv.config();
+
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "NewPassword@123",
-  database: "auth_db",
-  waitForConnections:true,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  ssl: {
+    ca: fs.readFileSync("ca.pem")
+  }
 });
+
 export default pool;
-
-
